3b74bf19572fc42fa38be7571cdc30ef17c99084
gnmmarechal <gnmpolicemata91@gmail.com>
2016-04-14 22:44:07 +0100

    Initial commit

---
+++ index.lua
@@ -0,0 +1,19 @@
+--Frankenfirm Updater Tool
+--
+--This tool uses Lua Player Plus 3DS with svchax support. It *could* potentially brick your 3DS!
+--
+--If you're not familiar with the terms "Frankenfirmware", "NVer", "CIA", "NAND" or "NATIVE_FIRM", DO NOT USE THIS PRIOR TO RESEARCHING.
+--This *should* be foolproof, but you should take caution as well.
+--
+-- Now, enjoy this. ~gnmmarechal/gnmpolicemata
+
+
+
+
+--Primary checks (extremely important)
+
+
+
+while true do
+
+end
