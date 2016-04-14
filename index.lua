e9650983d5c260d1df969fc1a4d0844ec2b92f83
gnmmarechal <gnmpolicemata91@gmail.com>
2016-04-14 23:25:35 +0100

    Added more stuff

--- index.lua
+++ index.lua
@@ -7,12 +7,18 @@
 --
 -- Now, enjoy this. ~gnmmarechal/gnmpolicemata
 
+white = Color.new(255,255,255)
+green = Color.new(0,240,32)
+red = Color.new(255,0,0)
 
 
+--Important functions
 
-function systemcheck() --Checks firmware version (major, minor, rev) and system region (USA, EUR, JPN)
-	major, minor, rev = System.getFirmware()
-	regint = System.getRegion()
+rel = 0 --1 means it will install the CIAs, 0 is merely a test for the interface itself, it won't change any files.
+
+function systemcheck() --Checks firmware version (major, minor, rev) , system region (USA, EUR, JPN) and model (0 = OLD, 1 = NEW)
+	major, minor, rev = System.getFirmware() --gets firmware version
+	regint = System.getRegion() --gets region
 	if regint == 1 then
 		region = "USA"
 	else
@@ -21,6 +27,28 @@ function systemcheck() --Checks firmware version (major, minor, rev) and system 
 		else
 			region = "JPN"
 		end	
+	end
+	modeln = System.getModel() --gets model
+	if modeln == 0 or model == 1 or model == 3 then
+		model = 0
+		modelstring = "Old 3DS/2DS"
+	else
+		model = 1
+		modelstring = "New 3DS"
+	end
+		
+end
+
+function displaysystem() -- Displays system information
+	debugPrint(0,0,"MAKE SURE THIS IS CORRECT!", red, BOTTOM_SCREEN)
+	debugPrint(0,20,"Abort if wrong! (XL doesn't matter)", red, BOTTOM_SCREEN)
+	debugPrint(0,20,"Model:"..modelstring, white, BOTTOM_SCREEN)
+end
+
+--other functions
+function checkquit()
+	if Controls.check(pad,KEY_B) and not Controls.check(oldpad,KEY_B) then
+		System.exit()
 	end
 end
 
