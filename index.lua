5ad8aaa0335d6bc67a63f1744dab3a07e3ffe3b9
gnmmarechal <gnmpolicemata91@gmail.com>
2016-04-15 20:52:47 +0100

    Script should now be 100% functional. Added n3DS Title IDs and fixed a bug in the syntax of installCIA

--- index.lua
+++ index.lua
@@ -1,12 +1,20 @@
---Frankenfirm Updater Tool
+--Frankenfirm Updater Tool (FrankenUpdater)
 --
 --This tool uses Lua Player Plus 3DS with svchax support. It *could* potentially brick your 3DS!
+--
+--Prior to using this, make sure your SD Card is ok and doesn't corrupt files out of the blue, try to download the CIAs multiple times and make sure they're the same file, and make sure the CIAs copy without issues.
 --
 --If you're not familiar with the terms "Frankenfirmware", "NVer", "CIA", "NAND" or "NATIVE_FIRM", DO NOT USE THIS PRIOR TO RESEARCHING.
 --This *should* be foolproof, but you should take caution as well.
 --
 -- Now, enjoy this. ~gnmmarechal/gnmpolicemata
-
+--
+--
+-- Credits:
+--
+-- *gnmmarechal/gnmpolicemata - Creator of the script itself
+-- *Rinnegatamante - Creator of Lua Player Plus 3DS
+--
 
 --Indicates the version of FrankenUpdater
 scriptver = "0.1"
@@ -19,13 +27,13 @@ red = Color.new(255,0,0)
 
 System.currentDirectory("/") -- Sets the current directory
 root = System.currentDirectory() --Defines the directory "root" as the root of the SDMC
-updatedir = root.."cia" --Defines the directory containing the CIA update files
+updatedir = root.."cia/" --Defines the directory containing the CIA update files
 scr = 1 --Defines the starting screen
 installed = 0 --Sets whether the CIA files have already been installed or not
 
 --Important functions
 
-rel = 0 --1 means it will install the CIAs, 0 is merely a test for the interface itself, it won't change any files.
+rel = 1 --1 means it will install the CIAs, 0 is merely a test for the interface itself, it won't change any files.
 
 function systemcheck() --Checks firmware version (major, minor, rev) , system region (USA, EUR, JPN) and model (0 = OLD, 1 = NEW)
 	major, minor, rev = System.getFirmware() --gets firmware version
@@ -100,22 +108,22 @@ end
 
 function newregionsetcia() -- Same as oldregionsetcia(), for the new3DS
 	if region == "USA" then
-		nver = ""
-		friends = ""
-		eshop = ""
-		mint = ""
+		nver = "000400DB20016302" --latest ver 512 (10.7)
+		friends = "0004013000003202" --latest ver 10240 (10.7)
+		eshop = "0004001000021900" --latest ver 21506 (10.7)
+		mint = "000400300000CE02" --latest ver 16384 (10.7)
 	end
 	if region == "EUR" then
-		nver = ""
-		friends = ""
-		eshop = ""
-		mint = ""
+		nver = "000400DB20016102" --latest ver 512 (10.7)
+		friends = "0004013000003202" --latest ver 10240 (10.7)
+		eshop = "0004001000022900" --latest ver 21505 (10.7)
+		mint = "000400300000D602" --latest ver 16384 (10.7)
 	end
 	if region == "JPN" then
-		nver = ""
-		friends = ""
-		eshop = ""
-		mint = ""
+		nver = "000400DB20016202" --latest ver 512 (10.7)
+		friends = "0004013000003202" --latest ver 10240 (10.7)
+		eshop = "0004001000020900" --latest ver 21504 (10.7)
+		mint = "000400300000C602" --latest ver 16384 (10.7)
 	end
 end
 
@@ -163,7 +171,17 @@ function installcia() --installs the CIA files to NAND (doesn't do anything if r
 	end
 end
 
-
+function optnrtrn() --Waits for SELECT then toggles between SCR 1 and SCR 3
+	if Controls.check(pad, KEY_SELECT) and not Controls.check(oldpad, KEY_SELECT) then
+		if scr==1 then
+			scr=3
+		else
+			if scr==3
+				scr=1
+			end
+		end
+	end
+end
 
 oldpad = Controls.read()
 
@@ -198,11 +216,19 @@ function startinstall() --scr = 2 --Second screen, starts the installation of th
 	debugPrint(0,40,"Installation started. Please wait..., white, TOP_SCREEN)
 	debugPrint(0,60,"DO NOT TURN THE POWER OFF!!!", red, TOP_SCREEN)
 	installcia()
-	debugPrint(0,80,"Done! Press B to quit!", white, TOP_SCREEN)
-	checkquit() 
+	debugPrint(0,80,"Done! Press B to quit!", white, TOP_SCREEN) 
 end
 
-
+function showciainfo() --scr = 3 --show the information about the CIA files
+	debugPrint(0,0,"FrankenUpdater v."..scriptver.." by gnmmarechal", white, TOP_SCREEN)
+	debugPrint(0,20,"===============", red, TOP_SCREEN)
+	debugPrint(0,40,"CIA Title Info:", white, TOP_SCREEN)
+	debugPrint(0,60,"NVer:"..nver, white, TOP_SCREEN)
+	debugPrint(0,80,"F. Module:"..friends, white, TOP_SCREEN)
+	debugPrint(0,100,"eShop:"..eshop, white, TOP_SCREEN)
+	debugPrint(0,120,"Mint:"..mint, white, TOP_SCREEN)
+	debugPrint(0,140,"Press SELECT to go back", white, TOP_SCREEN)
+end
 while true do
 	clear()
 	pad = Controls.read()
@@ -212,11 +238,16 @@ while true do
 	end
 	if scr == 1 then
 		disclaimer()
+		optnrtrn()
 	end
 	if scr == 2 then
 		startinstall()
+		checkquit()
 	end 
-	
+	if scr == 3 then
+		showciainfo()
+		optnrtrn()
+	end
 	
 	
 	Screen.flip()
